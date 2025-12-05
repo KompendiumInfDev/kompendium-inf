9eaf94c26c7d77775bef65b5b1ad862738ee0323
Kapios010 <67257824+Kapios010@users.noreply.github.com>
2025-12-05 06:03:21 +0100

    feat: Edytuj stronę główną

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="text-5xl font-bold mb-4">Kompendium Maturalne z Informatyki</h1>
+      <h1 className="text-5xl font-bold mb-4">Twoje Kompendium Maturalne z Informatyki</h1>
       <p>
-        Darmowe, kompletne, open-source.
+        Darmowe, kompletne&#42;, open-source.
       </p>
     </div>
     
